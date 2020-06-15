91e41d6b34de987a94ff06b150cabc863c506551
Amalia <58994466+akaresh@users.noreply.github.com>
2020-06-15 15:46:50 -0700

    updated gc.py
    "changes"#

--- MCB185-master/Lesson03/gc.py
+++ MCB185-master/Lesson03/gc.py
@@ -6,9 +6,18 @@
 
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT' # feel free to change
 
-i = 0
-for base in dna:
-    if
+num_of_GC = 0
+for i in range(len(dna)):
+    if dna[i] =='G' or dna[i] == 'C':
+        num_of_GC += 1
+    else:
+        pass
+
+content = num_of_GC/len(dna)
+
+print('%.2f' % (content))
+print('{:.2f}'.format(content))
+print(f'{content:.2f}')
 """
 python3 gc.py
 0.42
