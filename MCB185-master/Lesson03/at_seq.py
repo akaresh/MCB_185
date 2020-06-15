dd93c004776bebda15fe3470ced2b17913883cff
Amalia <58994466+akaresh@users.noreply.github.com>
2020-06-15 15:23:59 -0700

    updated at_seq.py
    "changes"

--- MCB185-master/Lesson03/at_seq.py
+++ MCB185-master/Lesson03/at_seq.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 import random
-random.seed(1) # comment-out this line to change sequence each time
+#random.seed(1) # comment-out this line to change sequence each time
 
 # Write a program that stores random DNA sequence in a string
 # The sequence should be 30 nt long
 # On average, the sequence should be 60% AT
 # Calculate the actual AT fraction while generating the sequence
 # Report the length, AT fraction, and sequence
+
+length = 30
+content = 0
+dna = ''
+for i in range(length):
+    r = random.randint(1, 4) # generate a random number from 1 to 4
+    if   r == 1:
+        dna += 'A'
+    elif r == 2:
+        dna +='C'
+    elif r == 3:
+        dna += 'G'
+    elif r == 4:
+        dna += 'T'
+    if (dna[i-1]+dna[i]) == 'AT':
+        content +=2
+
+print(length, dna, content, (content/length)*100)
+
 
 
 """
