9fc97bec428ca1e5bef61877fcc61c1e7f592cc0
Amalia <58994466+akaresh@users.noreply.github.com>
2020-06-17 09:43:34 -0700

    updated frame.py v2
    "changes"

--- MCB185-master/Lesson02/frame.py
+++ MCB185-master/Lesson02/frame.py
@@ -8,14 +8,22 @@ dna = 'ATGGCCTTT'
 
 
 ###single loop
+print("single loop v1")
 n = int(len(dna)/3)
-frame = list(range(3))*n
+frame = list(range(3))*n ###
 
 for a in range(0, len(dna)):
     print(a, frame[a], dna[a])
 
+print("single loop v2")
+
+
+for a in range(0, len(dna)):
+    n = a%3
+    print(a, frame[n], dna[a])
 
 ###nested loop
+print("nested loop")
 x = 0
 for i in range (0, len(dna), 3):
     for j in range(0, 3):
