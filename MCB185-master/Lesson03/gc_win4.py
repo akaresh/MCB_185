7e40167e894a7e3833b9648c87685db87bc56c93
Amalia <58994466+akaresh@users.noreply.github.com>
2020-06-26 16:18:20 -0700

    updates to gc_win4
    "changes"

--- MCB185-master/Lesson03/gc_win4.py
+++ MCB185-master/Lesson03/gc_win4.py
@@ -10,11 +10,30 @@ seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
 w = 11
 s = 5
 
+import re
+gc_content = 0
+
+for j in range(w):
+    if seq[j] == 'G' or seq[j] == 'C':
+        gc_content += 1
+print(f'0 {seq[:w]} {gc_content/w:.4f}')
+
+for i in range(s, len(seq)-w+1, s):
+    gc_content -= seq.count('G',i-s, i) + seq.count('C', i-s, i)
+    #print(seq[i-s:i])
+    gc_content += seq.count('G', i+w-s, i+w) + seq.count('C', i+w-s, i+w)
+    #print(seq[i+w-s:i+w])
+
+    #stripped = seq[i:i+w]
+
+    #gc_content = len(re.findall('C|G', stripped))
+    print(i, seq[i:i+w], f'{gc_content/w:.4f}')
+
 
 """
 python3 gc_win4.py
-0 ACGACGCAGGA 0.6364
-5 GCAGGAGGAGA 0.6364
+0 ACGAC GCAGGA 0.6364
+5 GCAGGA GGAGA 0.6364
 10 AGGAGAGTTTC 0.4545
 15 AGTTTCAGAGA 0.3636
 20 CAGAGATCACG 0.5455
