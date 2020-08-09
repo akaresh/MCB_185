866248895e061c3a79e07ee64a43f99171616b93
Amalia <58994466+akaresh@users.noreply.github.com>
2020-08-08 19:24:57 -0700

    23andme practice
    "changes"

--- MCB185-master/Lesson04/proc23.py
+++ MCB185-master/Lesson04/proc23.py
@@ -12,3 +12,47 @@ import fileinput
 """
 proc23.py whatever.23andme.whatever
 """
+
+data = []
+data_mt_DNA = []
+for line in fileinput.input():
+    if line[0] == '#':
+        continue
+    line = line.rstrip() # remove newline (return character), often useful
+    l = line.split('\t')
+    if l[1] != 'Y' and l[1] != 'MT':
+        data.append(str(l[3]))
+    if l[1] == 'MT':
+        data_mt_DNA.append(str(l[3]))
+
+#print(len(data))
+mt_DNA = 0
+failure_mt_DNA = 0
+SNP = 0
+failure = 0
+heterozygosity = 0
+
+for bps in data:
+    #print(bps, len(bps))
+    #try without .isalpha
+    #if not '-' in bps:
+    # check {A, C, G, T}
+    if bps[0] != '-' or bps[1] != '-':
+    #if bps.isalpha() is True:
+        SNP += 1
+        if bps[0] != bps[1]:
+            heterozygosity += 1
+        else:
+            continue
+    elif bps.isalpha() == False and len(bps) == 2: ###
+        failure +=1
+
+for b in data_mt_DNA:
+    if b.isalpha() == True and len(b) == 1: ###
+        mt_DNA += 1
+    elif b.isalpha() == False and len(b) == 2: ###
+        failure_mt_DNA += 1
+print('SNP:', SNP, 'Failure:', failure, 'MT_DNA:', mt_DNA,
+      'MT_DNA_Failure:', failure_mt_DNA, 'Heterozygous:', heterozygosity, "% Heterozygous:", heterozygosity/SNP)
+#print(data)
+    #data.append(float(line))
